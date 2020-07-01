e6f3caeb1597e1857fab4a9316cfd2a97f794a35
Tibor Nagy <xnagytibor@gmail.com>
2020-07-01 15:47:30 +0200

    Replace notify with inotify At least this crate works.

--- src/rom_watcher.rs
+++ src/rom_watcher.rs
@@ -1,68 +1,52 @@
+use std::io::Result;
 use std::sync::{Arc, Mutex};
+use std::thread;
 
-use notify::event::{AccessKind, AccessMode, CreateKind, ModifyKind, RemoveKind, RenameMode};
-use notify::{Event, EventKind, RecommendedWatcher, RecursiveMode, Result, Watcher};
+use inotify::{EventMask, Inotify, WatchMask};
 
 use crate::rom_manager::RomManager;
 
 pub struct RomWatcher {
     #[allow(dead_code)]
-    watcher: RecommendedWatcher,
+    inotify: Arc<Mutex<Inotify>>,
 }
 
 impl RomWatcher {
     pub fn new(rom_manager: Arc<Mutex<RomManager>>) -> Result<Self> {
-        let mut watcher: RecommendedWatcher = {
-            let rom_manager = rom_manager.clone();
-            Watcher::new_immediate(move |result: Result<Event>| {
-                //eprintln!("{:?}", result);
-
-                match result {
-                    Ok(event) => match event.kind {
-                        // Delete, Modify, Rename/move out of directory
-                        EventKind::Remove(RemoveKind::File)
-                        | EventKind::Access(AccessKind::Close(AccessMode::Write)) // Causes dupe refreshes with the workaround.
-                        | EventKind::Modify(ModifyKind::Name(RenameMode::From)) => {
-                            rom_manager.lock().unwrap().refresh().unwrap();
-                        }
-                        EventKind::Create(CreateKind::File) => {
-                            // The notify crate is borked on Linux, it fucks up rename events
-                            // when the source is outside of the watched directory.
-                            // It incorrectly reports those events as create events.
-                            // According to the EventKind docs this is not the right behaviour,
-                            // but I got too tired of reporting/arguing about these things.
-                            // They will 100% notice things are fucked when they decide to add
-                            // proper, real world test cases to their crate.
-                            // Until then, just hammer it and move on.
-                            {
-                                use std::fs::File;
-                                use std::{thread, time};
-
-                                eprintln!("Executing workaround for notify's brain damage");
-                                thread::sleep(time::Duration::from_millis(1500));
-
-                                assert_eq!(event.paths.len(), 1);
-                                let f = File::open(&event.paths[0]).unwrap();
-                                if f.metadata().unwrap().len() == 0 {
-                                    return;
-                                }
-                            }
-
-                            rom_manager.lock().unwrap().refresh().unwrap();
-                        }
-                        _ => {}
-                    },
-                    Err(e) => eprintln!("watch error: {:?}", e),
-                }
-            })?
-        };
+        let inotify = Arc::new(Mutex::new(Inotify::init()?));
 
         {
             let rom_manager = rom_manager.lock().unwrap();
             let base_directory = rom_manager.base_directory.as_path();
-            watcher.watch(&base_directory, RecursiveMode::NonRecursive)?;
+            inotify
+                .lock()
+                .unwrap()
+                .add_watch(base_directory, WatchMask::ALL_EVENTS)?;
         }
 
-        Ok(Self { watcher })
+        {
+            let inotify = inotify.clone();
+            thread::spawn(move || {
+                let mut buffer = [0u8; 4096];
+                loop {
+                    let events = inotify.lock().unwrap().read_events_blocking(&mut buffer).unwrap();
+
+                    let mut changed = false;
+
+                    for event in events {
+                        changed |= event.mask.contains(EventMask::MOVED_FROM);
+                        changed |= event.mask.contains(EventMask::MOVED_TO);
+                        changed |= event.mask.contains(EventMask::DELETE);
+                        changed |= event.mask.contains(EventMask::CLOSE_WRITE);
+                    }
+
+                    if changed {
+                        rom_manager.lock().unwrap().refresh().unwrap();
+                    }
+                }
+            });
+        }
+
+        Ok(Self { inotify })
     }
 }
